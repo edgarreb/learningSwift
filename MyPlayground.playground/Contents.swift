b6e20d0d191e1afd8800127365daea0b037d389c
Edgar Martinez <edgarmarreb@gmail.com>
2020-01-06 21:26:11 -0800

    Ground Zero

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let someCharacter: Character = "z"
+
+switch someCharacter {
+case "a":
+    print("The first letter of the alphabet")
+case "z":
+    print("The last letter of the alphabet")
+default:
+    print("Some other character")
+}
+
+for x in 1...5 {
+    
+    print(x)
+}
